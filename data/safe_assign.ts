724a1b3cf78a5fc6e0c1f4fe9b4df114be00f50a
Natasha Osborne <admin@natashaosborne.com>
2024-08-29 14:40:59 -0600

    convert safe_assign to ts

--- data/safe_assign.ts
+++ data/safe_assign.ts
@@ -1,13 +1,13 @@
-function safeAssign(target, source) {
+import {Context} from '../commands/command';
+
+export default function safeAssign(target: Context, source: Context) {
   for (let key in source) {
     if (source.hasOwnProperty(key)) {
       try {
         target[key] = source[key];
-      } catch (error) {
+      } catch (error: any) {
         console.warn(`Skipping ${key}:`, error.message);
       }
     }
   }
 }
-
-module.exports = safeAssign;
